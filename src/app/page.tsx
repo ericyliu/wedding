857665cf54b4ee92e712fca69fd75eecf33d82bc
Eric Liu <eyliubit@gmail.com>
2024-06-20 18:21:20 -0700

    added doors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,32 @@
+import Image from "next/image";
 import styles from "./page.module.css";
+import { motion } from "framer-motion";
 
 export default function Home() {
-  return <main className={styles.main}>Coming Soon</main>;
+  return (
+    <main className={styles.main}>
+      <div className={styles.content}>
+        <div className={styles.backgroundLantern}>
+          <Image src="/background-lantern.png" alt="lantern" fill />
+        </div>
+        <div className={styles.backgroundLantern}>
+          <Image src="/background-lantern.png" alt="lantern" fill />
+        </div>
+      </div>
+      <div className={styles.doorContainer}>
+        <div className={styles.door}>
+          <Image src="/door.png" alt="door" fill />
+        </div>
+        <div className={styles.door}>
+          <Image src="/door.png" alt="door" fill />
+        </div>
+        <div className={styles.door}>
+          <Image src="/door.png" alt="door" fill />
+        </div>
+        <div className={styles.door}>
+          <Image src="/door.png" alt="door" fill />
+        </div>
+      </div>
+    </main>
+  );
 }
