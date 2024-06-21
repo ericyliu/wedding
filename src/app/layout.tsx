fbf76ffd016e98bbdf211753a6b09d9865350514
Eric Liu <eyliubit@gmail.com>
2024-06-20 19:45:46 -0700

    add content

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Ma_Shan_Zheng } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const font1 = Ma_Shan_Zheng({ subsets: ["latin"], weight: "400" });
 
 export const metadata: Metadata = {
   title: "Eric <3 Angela",
@@ -16,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={font1.className}>{children}</body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Image from "next/image";
 import styles from "./page.module.css";
 import { motion } from "framer-motion";
@@ -5,24 +7,90 @@ import { motion } from "framer-motion";
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.content}>
-        <div className={styles.backgroundLantern}>
-          <Image src="/background-lantern.png" alt="lantern" fill />
+      <div className={styles.middle}>
+        <div className={styles.decoration}>
+          <div>
+            <div className={styles.backgroundLantern}>
+              <Image
+                className={styles.image}
+                src="/background-lantern.png"
+                alt="lantern"
+                fill
+              />
+            </div>
+            <div className={styles.backgroundLantern}>
+              <Image
+                className={styles.image}
+                src="/background-lantern.png"
+                alt="lantern"
+                fill
+              />
+            </div>
+          </div>
+          <div>
+            <div className={styles.foregroundLantern}>
+              <Image
+                className={styles.image}
+                src="/foreground-lantern.png"
+                alt="lantern"
+                fill
+              />
+            </div>
+            <div className={styles.foregroundLantern}>
+              <Image
+                className={styles.image}
+                src="/foreground-lantern.png"
+                alt="lantern"
+                fill
+              />
+            </div>
+          </div>
+          <div className={styles.loki}>
+            <Image className={styles.image} src="/loki.png" alt="loki" fill />
+          </div>
+          <div className={styles.teemo}>
+            <Image className={styles.image} src="/teemo.png" alt="loki" fill />
+          </div>
         </div>
-        <div className={styles.backgroundLantern}>
-          <Image src="/background-lantern.png" alt="lantern" fill />
+        <div className={styles.content}>
+          <h1 className={styles.h1}>囍</h1>
+          <div className={styles.couple}>
+            <Image
+              className={styles.image}
+              src="/couple.png"
+              alt="couple"
+              fill
+            />
+          </div>
+          <p className={styles.p1}>請和我們一起慶祝</p>
+          <p className={styles.p2}>
+            <b className={styles.bold}>刘宇辉</b> 和{" "}
+            <b className={styles.bold}>袁詠欣</b>
+          </p>
+          <div className={styles.info}>
+            <p>April 19, 2025</p>
+            <p>Queens, NY</p>
+          </div>
         </div>
       </div>
       <div className={styles.doorContainer}>
         <div className={styles.door}>
           <Image src="/door.png" alt="door" fill />
         </div>
-        <div className={styles.door}>
+        <motion.div
+          animate={{ x: "-100%" }}
+          transition={{ duration: 1.7 }}
+          className={styles.door}
+        >
           <Image src="/door.png" alt="door" fill />
-        </div>
-        <div className={styles.door}>
+        </motion.div>
+        <motion.div
+          animate={{ x: "100%" }}
+          transition={{ duration: 1.7 }}
+          className={styles.door}
+        >
           <Image src="/door.png" alt="door" fill />
-        </div>
+        </motion.div>
         <div className={styles.door}>
           <Image src="/door.png" alt="door" fill />
         </div>
