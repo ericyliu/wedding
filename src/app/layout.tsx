330ee6f9ab29adcd87f60b2a8cde1cec0d591c01
Eric Liu <eyliubit@gmail.com>
2024-06-20 20:01:09 -0700

    add font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=LXGW+WenKai+TC&display=swap"
+          rel="stylesheet"
+        />
+      </head>
       <body className={font1.className}>{children}</body>
     </html>
   );
